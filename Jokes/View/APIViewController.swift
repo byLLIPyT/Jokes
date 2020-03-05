1b80d9d1be488f5f8b6924c900e597c6f7a76a59
byLLIPyT <byllipyt@icloud.com>
2020-03-05 23:59:35 +0300

    +API screen

---
+++ Jokes/View/APIViewController.swift
@@ -0,0 +1,25 @@
+//
+//  APIViewController.swift
+//  Jokes
+//
+//  Created by Александр Уткин on 05.03.2020.
+//  Copyright © 2020 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class APIViewController: UIViewController {
+        
+    @IBOutlet weak var webView: WKWebView!
+    
+    private let mainUrl = "http://www.icndb.com/api/"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let url = URL(string: mainUrl) else { return }
+        
+        self.webView.load(URLRequest(url: url))
+    }
+}
